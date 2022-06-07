6a6da1167c2e06a0320bfec9dc2a2aa22849d970
unknown <you@example.com>
2022-06-07 16:21:15 -0700

    feat 1 and 2

---
+++ main.js
@@ -0,0 +1,22 @@
+
+    
+//     function convert() {
+//     let inputValue = document.getElementById('inputVal').value;
+//     let ouputValue = document.querySelector('.output');
+//     let math = inputValue * 2;
+    
+//     console.log(inputValue);
+    
+//     ouputValue.innerHTML = math;
+// }
+
+
+    function convertclick() {
+    let inputValue = document.getElementById('inputVal').value;
+    let ouputValue = document.querySelector('.output');
+    let math = inputValue * 2;
+    
+    console.log(inputValue);
+    
+    ouputValue.textContent = math;
+}
