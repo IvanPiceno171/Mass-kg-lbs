ed66f269e0c9544f5cae8fc7813d10ca88f8884c
unknown <you@example.com>
2022-06-14 14:40:04 -0700

    cleaned up code

--- main.js
+++ main.js
@@ -4,18 +4,12 @@
     let inputValue = document.getElementById('inputVal').value;
     let ouputValue = document.querySelector('.output');
     let math = inputValue * 2.20462262185;
-    
-    console.log(inputValue);
-    
     ouputValue.textContent = math;
 }
-
 // on click function with if else statement
 function convertMiles(){
     let inputValue2 = document.getElementById('inputVal2').value;
     let ouputValue2 = document.querySelector('.output2');
-    // let math2 = inputValue2 * 2;
-
     if(isNaN(inputValue2)){
         alert('not a number, Enter a number')
     }
